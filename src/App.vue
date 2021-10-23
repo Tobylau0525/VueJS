f449e6e5668ab2dbd7ee3131e3676e9ea7e91765
unknown <tobylau010525@gmail.com>
2021-10-23 03:13:08 -0400

    code along, created Event cards with style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
-<template>
+ <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+
     <router-view />
   </div>
 </template>
@@ -28,5 +29,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4{
+  font-size: 20px;
 }
 </style>
