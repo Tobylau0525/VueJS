be76c2bedb8b82b403061a794b476d2c3835f45a
unknown <tobylau010525@gmail.com>
2021-10-24 09:42:39 -0400

    Used Dynamic routing for event card

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,10 +1,11 @@
 <template>
+<router-link class="event-link" :to="{ name: 'EventDetails', params:{id: event.id}}">
   <div class="event-card">
     <!-- Display event data -->
     <span>@{{ event.time}} on {{event.date}}</span>
     <h4>{{event.title}}</h4>
-
   </div>
+</router-link>
 </template>
 
 <script>
@@ -30,4 +31,8 @@ export default {
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 
+.event-link{
+  color: #2c3e50;
+  text-decoration: none;
+}
 </style>
